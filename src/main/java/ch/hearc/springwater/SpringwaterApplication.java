451e1806f9de372457b804985a39e695691d52de
bull0n <lucas.bulloni@protonmail.ch>
2019-02-20 15:14:29 +0100

    first commit

---
+++ src/main/java/ch/hearc/springwater/SpringwaterApplication.java
@@ -0,0 +1,13 @@
+package ch.hearc.springwater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwaterApplication.class, args);
+	}
+
+}
