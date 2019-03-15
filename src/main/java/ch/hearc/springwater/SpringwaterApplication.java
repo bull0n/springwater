6d2a4a700f1f0cefa76d5bdcdcae2f354e35c9a9
Bastien Wermeille <bastien.wermeille@gmail.com>
2019-03-15 08:50:26 +0100

    Improve code smell

--- src/main/java/ch/hearc/springwater/SpringwaterApplication.java
+++ src/main/java/ch/hearc/springwater/SpringwaterApplication.java
@@ -1,25 +1,16 @@
 package ch.hearc.springwater;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import ch.hearc.springwater.models.entities.Categorie;
 import ch.hearc.springwater.models.repositories.CategoriesRepository;
 
 @SpringBootApplication
 public class SpringwaterApplication {
-	
+
 	@Autowired
 	public CategoriesRepository repository;
-	
-	@PostConstruct
-	public void init()
-	{
-
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringwaterApplication.class, args);
