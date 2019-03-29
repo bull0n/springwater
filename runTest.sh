1f99b4b008926df27f51a637b801996db5577b49
bull0n <lucas.bulloni@protonmail.ch>
2019-03-29 08:17:44 +0100

    jenkins

--- runTest.sh
+++ runTest.sh
@@ -4,4 +4,4 @@ Xvfb :99 &
 export local_addr="127.0.0.1:8080"
 
 
-/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash -runMode=console -projectPath="$(pwd)/TestsKatalon/springwater/springwater.prj" -retry=1 -testSuitePath="Test Suites/main" -executionProfile="default" -browserType="Firefox (headless)" -Djava.awt.headless
+/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash -runMode=console -projectPath="$(pwd)/TestsKatalon/springwater/springwater.prj" -retry=1 -testSuitePath="Test Suites/main" -executionProfile="default" -browserType="Chrome (headless)" -Djava.awt.headless
