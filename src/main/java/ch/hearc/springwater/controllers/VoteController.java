9a9e1b90b627656e6e66b98017cc8623fcc60b69
Asmoday4949 <malik.fleury@bluewin.ch>
2019-04-15 15:38:29 +0200

    Add vote controller

---
+++ src/main/java/ch/hearc/springwater/controllers/VoteController.java
@@ -0,0 +1,95 @@
+package ch.hearc.springwater.controllers;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.hearc.springwater.exceptions.ResourceNotFoundException;
+import ch.hearc.springwater.models.entities.Boisson;
+import ch.hearc.springwater.models.entities.Vote;
+import ch.hearc.springwater.models.repositories.BoissonsRepository;
+import ch.hearc.springwater.models.repositories.VoteRepository;
+import ch.hearc.springwater.security.Utilisateur;
+import ch.hearc.springwater.security.UtilisateurRepository;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping(value = "/vote")
+public class VoteController {
+	private int SCORE_UP = 1;
+	private int SCORE_DOWN = -1;
+
+	@Autowired
+	UtilisateurRepository utilisateurRepository;
+
+	@Autowired
+	BoissonsRepository boissonRepository;
+
+	@Autowired
+	VoteRepository voteRepository;
+
+	@PostMapping(value = "/upvote/{id}")
+	public ResponseEntity<String> UpVote(@PathVariable Long id, Model model) {
+		this.vote(id, model, SCORE_UP);
+		return new ResponseEntity<>("", HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/downvote/{id}")
+	public ResponseEntity<String> DownVote(@PathVariable Long id, Model model) {
+		this.vote(id, model, SCORE_DOWN);
+		return new ResponseEntity<>("", HttpStatus.OK);
+	}
+
+	public void vote(Long id, Model model, int score) {
+		Authentication authentification = SecurityContextHolder.getContext().getAuthentication();
+
+		if (authentification.isAuthenticated()) {
+			String nom = authentification.getName();
+			Utilisateur utilisateurCourant = utilisateurRepository.findByNomUtilisateur(nom);
+			Boisson boisson = boissonRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+
+			List<Vote> listeVotes = utilisateurCourant.getVotes();
+			Vote vote = this.getVoteFromList(id, listeVotes);
+			
+			if(vote == null)
+			{
+				Vote nouveauVote = new Vote();
+				nouveauVote.setScore(score);
+				nouveauVote.setBoisson(boisson);
+				nouveauVote.setUser(utilisateurCourant);
+				listeVotes.add(nouveauVote);
+				utilisateurRepository.save(utilisateurCourant);
+			}
+			else if(vote.getScore() != score)
+			{
+				vote.setScore(score);
+				voteRepository.save(vote);
+			}
+		}
+	}
+
+	public Vote getVoteFromList(Long id, List<Vote> listeVotes) {
+		for (Vote vote : listeVotes) {
+			if(vote.getBoisson().getId() == id) {
+				return vote;
+			}
+		}
+		
+		return null;
+	}
+}
