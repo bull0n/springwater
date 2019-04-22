fe60b9e1cac2674698bd2371d46b72026c7ecf63
Bastien Wermeille <bastien.wermeille@gmail.com>
2019-04-22 15:16:59 +0200

    Add test for boisson repo

--- src/test/java/ch/hearc/springwater/model/repositories/BoissonRepositoryTest.java
+++ src/test/java/ch/hearc/springwater/model/repositories/BoissonRepositoryTest.java
@@ -3,8 +3,12 @@ package ch.hearc.springwater.model.repositories;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
+import org.assertj.core.util.Arrays;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +17,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import ch.hearc.springwater.models.entities.Boisson;
+import ch.hearc.springwater.models.entities.Categorie;
 import ch.hearc.springwater.models.repositories.BoissonsRepository;
 
 @RunWith(SpringRunner.class)
@@ -38,10 +43,89 @@ public class BoissonRepositoryTest {
 		Optional<Boisson> boissonRecherche = boissonRepository.findById(boisson.getId());
 		
 		assertTrue(boissonRecherche.isPresent());
-		assertTrue(boisson.getId().equals(boissonRecherche.get().getId()));
 		assertThat(boissonRecherche.get()).isNotNull();
+		assertTrue(boisson.getId().equals(boissonRecherche.get().getId()));
 	}
 	
-	//TODO:
+	@Test
+	public void givenBoisson_whenPersistBoisson_theBoissonHasRightName() {
+		String name = "SpringWater made with love";
+		Boisson boisson = new Boisson();
+		boisson.setNom(name);
+		boisson.setDescription("Best drink ever looks like");
+		boisson.setFileURL("");
+		
+		entityManager.persist(boisson);
+		entityManager.flush();
+		
+		Optional<Boisson> boissonRecherche = boissonRepository.findById(boisson.getId());
+		
+		assertTrue(boissonRecherche.isPresent());
+		assertThat(boissonRecherche.get()).isNotNull();
+		assertTrue(name.equals(boissonRecherche.get().getNom()));
+	}
+	
+	@Test
+	public void givenBoisson_whenPersistBoisson_theBoissonHasRightDescription() {
+		String description = "Best drink ever for sure";
+		Boisson boisson = new Boisson();
+		boisson.setNom("Soda");
+		boisson.setDescription(description);
+		boisson.setFileURL("");
+		
+		entityManager.persist(boisson);
+		entityManager.flush();
+		
+		Optional<Boisson> boissonRecherche = boissonRepository.findById(boisson.getId());
+		
+		assertTrue(boissonRecherche.isPresent());
+		assertThat(boissonRecherche.get()).isNotNull();
+		assertTrue(description.equals(boissonRecherche.get().getDescription()));
+	}
+	
+	@Test
+	public void givenBoisson_whenPersistBoisson_theBoissonHasRightCategories() {
+		String description = "Best drink ever for sure";
+		Boisson boisson = new Boisson();
+		boisson.setNom("Soda");
+		boisson.setDescription(description);
+		
+		List<Object> listCategoriesNameLinked = Arrays.asList(new String[]{"vodka", "soda", "Test", "Rongob"});
+		List<Object> listCategoriesNameNotLinked = Arrays.asList(new String[]{"Fromage", "Raddis", "Youpoulala"});
+		
+		Set<Categorie> setCategories = new HashSet<>();
+		for(Object name : listCategoriesNameLinked) {
+			Categorie categorie = new Categorie();
+			categorie.setNom((String)name);
+			entityManager.persist(categorie);
+			entityManager.flush();
+			setCategories.add(categorie);
+		}
 
+		for(Object name : listCategoriesNameNotLinked) {
+			Categorie categorie = new Categorie();
+			categorie.setNom((String)name);
+			entityManager.persist(categorie);
+			entityManager.flush();
+		}
+		
+		boisson.setCategories(setCategories);
+		boisson.setFileURL("");
+		
+		entityManager.persist(boisson);
+		entityManager.flush();
+		
+		Optional<Boisson> boissonRecherche = boissonRepository.findById(boisson.getId());
+		
+		assertTrue(boissonRecherche.isPresent());
+		assertThat(boissonRecherche.get()).isNotNull();
+		assertTrue(setCategories.equals(boissonRecherche.get().getCategories()));
+	}
+	
+	@Test
+	public void givenBoisson_whenPersistBoisson_theBoissonHasRightVotes() {
+		//TODO: 
+	}
+	
+	
 }
