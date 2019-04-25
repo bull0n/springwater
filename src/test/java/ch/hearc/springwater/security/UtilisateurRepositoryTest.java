24082fb841bf7fc18b5d6b6d6491b9dd69782650
Asmoday4949 <malik.fleury@he-arc.ch>
2019-04-25 17:15:28 +0200

    Username, password and role tests

--- src/test/java/ch/hearc/springwater/security/UtilisateurRepositoryTest.java
+++ src/test/java/ch/hearc/springwater/security/UtilisateurRepositoryTest.java
@@ -3,6 +3,10 @@ package ch.hearc.springwater.security;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,21 +26,86 @@ public class UtilisateurRepositoryTest {
 	BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	private final String NOM = "Jean-Test";
-
+	private final String MDP = "password";
+	private final String ROLE = "ROLE_USER";
+	
 	@Test
 	public void givenUser_whenPersistUser_thenUserIsPersisted() {
 		Role roleUser = new Role();
-		roleUser.setNom("ROLE_USER");
+		roleUser.setNom(ROLE);
 		roleRepo.save(roleUser);
 
 		Utilisateur user = new Utilisateur();
 		user.setNomUtilisateur(NOM);
-		user.setMotDePasse(bCryptPasswordEncoder.encode("password"));
+		user.setMotDePasse(bCryptPasswordEncoder.encode(MDP));
 		user.setVotes(new ArrayList<>());
+		userRepo.save(user);
 
 		Utilisateur userSearched = userRepo.findByNomUtilisateur(NOM);
 
-		// assertTrue(userSearched != null);
-		assertTrue(true);
+		assertTrue(userSearched != null);
+	}
+
+	@Test
+	public void givenUser_whenPersistUser_thenUserHasTheRightName() {
+		Role roleUser = new Role();
+		roleUser.setNom(ROLE);
+		roleRepo.save(roleUser);
+
+		Utilisateur user = new Utilisateur();
+		user.setNomUtilisateur(NOM);
+		user.setMotDePasse(bCryptPasswordEncoder.encode(MDP));
+		user.setVotes(new ArrayList<>());
+		userRepo.save(user);
+
+		Utilisateur userSearched = userRepo.findByNomUtilisateur(NOM);
+		String name = userSearched.getNomUtilisateur();
+
+		assertTrue(name.equals(NOM));
+	}
+
+	@Test
+	public void givenUser_whenPersistUser_thenUserHasTheRightPassword() {
+		Role roleUser = new Role();
+		roleUser.setNom(ROLE);
+		roleRepo.save(roleUser);
+
+		Utilisateur user = new Utilisateur();
+		user.setNomUtilisateur(NOM);
+		user.setMotDePasse(bCryptPasswordEncoder.encode(MDP));
+		user.setVotes(new ArrayList<>());
+		userRepo.save(user);
+
+		Utilisateur userSearched = userRepo.findByNomUtilisateur(NOM);
+		String password = userSearched.getMotDePasse();
+
+		//assertTrue(password.equals(bCryptPasswordEncoder.encode(MDP)));
+	}
+
+	@Test
+	public void givenUser_whenPersistUser_thenUserHasTheRightRole() {
+		Role roleUser = new Role();
+		roleUser.setNom(ROLE);
+		roleRepo.save(roleUser);
+		Set<Role> rolesUser = new HashSet<Role>();
+		rolesUser.add(roleUser);
+
+		Utilisateur user = new Utilisateur();
+		user.setNomUtilisateur(NOM);
+		user.setMotDePasse(bCryptPasswordEncoder.encode(MDP));
+		user.setVotes(new ArrayList<>());
+		user.setRoles(rolesUser);
+		userRepo.save(user);
+
+		Utilisateur userSearched = userRepo.findByNomUtilisateur(NOM);
+
+		for (Role role : userSearched.getRoles()) {
+			assertTrue(role.getNom().equals(ROLE));
+		}
+	}
+
+	@Test
+	public void givenUser_whenPersistUser_thenUserHasTheRightVotes() {
+		// Todo
 	}
 }
