49f0fb5387a757800fdb77bcb593ba443265ef03
Asmoday4949 <malik.fleury@bluewin.ch>
2019-04-25 18:06:16 +0200

    Use entityManager instead repo

--- src/test/java/ch/hearc/springwater/security/UtilisateurRepositoryTest.java
+++ src/test/java/ch/hearc/springwater/security/UtilisateurRepositoryTest.java
@@ -12,12 +12,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class UtilisateurRepositoryTest {
+	@Autowired
+	private TestEntityManager entityManager;
 	@Autowired
 	private UtilisateurRepository userRepo;
 	@Autowired
@@ -33,13 +36,13 @@ public class UtilisateurRepositoryTest {
 	public void givenUser_whenPersistUser_thenUserIsPersisted() {
 		Role roleUser = new Role();
 		roleUser.setNom(ROLE);
-		roleRepo.save(roleUser);
+		entityManager.persist(roleUser);
 
 		Utilisateur user = new Utilisateur();
 		user.setNomUtilisateur(NOM);
 		user.setMotDePasse(bCryptPasswordEncoder.encode(MDP));
 		user.setVotes(new ArrayList<>());
-		userRepo.save(user);
+		entityManager.persist(user);
 
 		Utilisateur userSearched = userRepo.findByNomUtilisateur(NOM);
 
@@ -50,13 +53,13 @@ public class UtilisateurRepositoryTest {
 	public void givenUser_whenPersistUser_thenUserHasTheRightName() {
 		Role roleUser = new Role();
 		roleUser.setNom(ROLE);
-		roleRepo.save(roleUser);
+		entityManager.persist(roleUser);
 
 		Utilisateur user = new Utilisateur();
 		user.setNomUtilisateur(NOM);
 		user.setMotDePasse(bCryptPasswordEncoder.encode(MDP));
 		user.setVotes(new ArrayList<>());
-		userRepo.save(user);
+		entityManager.persist(user);
 
 		Utilisateur userSearched = userRepo.findByNomUtilisateur(NOM);
 		String name = userSearched.getNomUtilisateur();
@@ -68,13 +71,13 @@ public class UtilisateurRepositoryTest {
 	public void givenUser_whenPersistUser_thenUserHasTheRightPassword() {
 		Role roleUser = new Role();
 		roleUser.setNom(ROLE);
-		roleRepo.save(roleUser);
+		entityManager.persist(roleUser);
 
 		Utilisateur user = new Utilisateur();
 		user.setNomUtilisateur(NOM);
 		user.setMotDePasse(bCryptPasswordEncoder.encode(MDP));
 		user.setVotes(new ArrayList<>());
-		userRepo.save(user);
+		entityManager.persist(user);
 
 		Utilisateur userSearched = userRepo.findByNomUtilisateur(NOM);
 		String password = userSearched.getMotDePasse();
@@ -86,7 +89,7 @@ public class UtilisateurRepositoryTest {
 	public void givenUser_whenPersistUser_thenUserHasTheRightRole() {
 		Role roleUser = new Role();
 		roleUser.setNom(ROLE);
-		roleRepo.save(roleUser);
+		entityManager.persist(roleUser);
 		Set<Role> rolesUser = new HashSet<Role>();
 		rolesUser.add(roleUser);
 
@@ -95,7 +98,7 @@ public class UtilisateurRepositoryTest {
 		user.setMotDePasse(bCryptPasswordEncoder.encode(MDP));
 		user.setVotes(new ArrayList<>());
 		user.setRoles(rolesUser);
-		userRepo.save(user);
+		entityManager.persist(user);
 
 		Utilisateur userSearched = userRepo.findByNomUtilisateur(NOM);
 
