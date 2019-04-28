3e88fa2108a2e5861117c10702e981ff559fcfeb
Bastien Wermeille <bastien.wermeille@gmail.com>
2019-04-28 12:50:31 +0200

    Add categoryRepositoryTest

--- src/test/java/ch/hearc/springwater/model/repositories/CategorieRepositoryTest.java
+++ src/test/java/ch/hearc/springwater/model/repositories/CategorieRepositoryTest.java
@@ -7,91 +7,31 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import ch.hearc.springwater.models.entities.Boisson;
-import ch.hearc.springwater.models.entities.Vote;
-import ch.hearc.springwater.security.Utilisateur;
+import ch.hearc.springwater.models.entities.Categorie;
+import ch.hearc.springwater.models.repositories.CategoriesRepository;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class CategorieRepositoryTest {
 
 	@Autowired
-	private TestEntityManager entityManager;
+	CategoriesRepository repository;
 
 	@Autowired
-	BCryptPasswordEncoder bCryptPasswordEncoder;
-
-	private static final int SCORE = 1;
+	private TestEntityManager entityManager;
 
 	@Test
-	@WithMockUser(username = "admin", roles = { "ROLE_ADMIN" })
-	public void givenCategorie_whenPersistCategorie_theCategorieHasBoissonList() {
-		Boisson boisson = new Boisson();
-		boisson.setNom("Soda");
-		boisson.setDescription("");
-		boisson.setFileURL("");
-		entityManager.persist(boisson);
+	@WithMockUser(username = "admin", roles = { "ADMIN" })
+	public void givenCategorie_whenPersistCategorie_theCategorieHasEmptyBoissonList() {
+		Categorie categorie = new Categorie();
+		categorie.setNom("Categorie one");
 
-		Utilisateur utilisateur = new Utilisateur();
-		utilisateur.setNomUtilisateur("Jean Emarre");
-		utilisateur.setMotDePasse(bCryptPasswordEncoder.encode("password"));
-		entityManager.persist(utilisateur);
+		Categorie persistedCategorie = entityManager.persist(categorie);
+		entityManager.flush();
 
-		Vote vote = new Vote();
-		vote.setScore(SCORE);
-		vote.setBoisson(boisson);
-		vote.setUser(utilisateur);
-		Vote persistedVote = entityManager.persist(vote);
-
-		assertTrue(persistedVote.getBoisson().getNom().equals(boisson.getNom()));
+		assertTrue(persistedCategorie.getNbBoisson() == 0);
 	}
-
-	@Test
-	public void givenVote_whenPersistVote_theVoteHasRightScore() {
-		Boisson boisson = new Boisson();
-		boisson.setNom("Soda");
-		boisson.setDescription("");
-		boisson.setFileURL("");
-		entityManager.persist(boisson);
-
-		Utilisateur utilisateur = new Utilisateur();
-		utilisateur.setNomUtilisateur("Jean Emarre");
-		utilisateur.setMotDePasse(bCryptPasswordEncoder.encode("password"));
-		entityManager.persist(utilisateur);
-
-		Vote vote = new Vote();
-		vote.setScore(SCORE);
-		vote.setBoisson(boisson);
-		vote.setUser(utilisateur);
-		Vote persistedVote = entityManager.persist(vote);
-
-		assertTrue(persistedVote.getScore() == SCORE);
-	}
-
-	@Test
-	public void givenVote_whenPersistVote_theVoteHasRightUtilisateur() {
-		Boisson boisson = new Boisson();
-		boisson.setNom("Soda");
-		boisson.setDescription("");
-		boisson.setFileURL("");
-		entityManager.persist(boisson);
-
-		Utilisateur utilisateur = new Utilisateur();
-		utilisateur.setNomUtilisateur("Jean Emarre");
-		utilisateur.setMotDePasse(bCryptPasswordEncoder.encode("password"));
-		entityManager.persist(utilisateur);
-
-		Vote vote = new Vote();
-		vote.setScore(SCORE);
-		vote.setBoisson(boisson);
-		vote.setUser(utilisateur);
-		Vote persistedVote = entityManager.persist(vote);
-
-		assertTrue(persistedVote.getUser().getNomUtilisateur().equals(utilisateur.getNomUtilisateur()));
-	}
-
 }
