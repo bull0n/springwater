564c84a440e751ccf9c812bee5b494776f3ca7e6
bull0n <lucas.bulloni@protonmail.ch>
2019-05-01 15:39:30 +0200

    katalons

--- TestsKatalon/springwater/Test Suites/main.ts
+++ TestsKatalon/springwater/Test Suites/main.ts
@@ -14,6 +14,18 @@
       <guid>9859475c-256c-42af-a428-980286a061a1</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/SignupLoginAdd</testCaseId>
+      <testCaseId>Test Cases/Signup</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bdfb0e7b-5dce-4ece-8051-0038eb360b78</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2e0f57db-62d2-4fa9-ba09-b81047b7c8b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Search</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
